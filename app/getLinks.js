ac6ccc4872441907d37a46774063bbc4bb96aee4
David <davidzxgan@gmail.com>
2023-08-04 21:53:53 -0400

    Git Github API to work

--- app/getLinks.js
+++ app/getLinks.js
@@ -1,18 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var request = require("request");
-function getUserInfo(userName) {
-    request.get('https://api.github.com/users/' + userName, function (response) {
-        console.log(response);
-        console.log("Hi");
-    });
-}
-getUserInfo("");
-// console.log("Hello World");
-// var topic = prompt("What topic do you want to explore?")
-// let topic = new String(prompt("What topic do you want to explore?"))
-// console.log("Message" + topic)
-// function getLink(topic){
-//     // console.log(topic)
-//     // need to get all links, so scrounge web
+var axios_1 = require("axios");
+axios_1.default.get('https://api.github.com/users/mapbox')
+    .then(function (response) {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+});
+// import * as request from 'request';
+// function getUserInfo(userName : string){
+//     request.get('https://api.github.com/users/' + userName, (response : string) =>{
+//         console.log(response);
+//         console.log("Hi");
+//     })
 // }
+// getUserInfo("")
--- app/getLinks.tsx
+++ app/getLinks.tsx
@@ -1,23 +1,22 @@
-import * as request from 'request';
+import axios from "axios"
 
-function getUserInfo(userName : string){
-    request.get('https://api.github.com/users/' + userName, (response : any) =>{
-        console.log(response);
-        console.log("Hi");
-    })
-}
-
-getUserInfo("")
-// console.log("Hello World");
-// var topic = prompt("What topic do you want to explore?")
-
-// let topic = new String(prompt("What topic do you want to explore?"))
-
-// console.log("Message" + topic)
-
-// function getLink(topic){
-//     // console.log(topic)
-//     // need to get all links, so scrounge web
+axios.get('https://api.github.com/users/mapbox')
+  .then((response) => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  });
 
 
+// import * as request from 'request';
+
+// function getUserInfo(userName : string){
+//     request.get('https://api.github.com/users/' + userName, (response : string) =>{
+//         console.log(response);
+//         console.log("Hi");
+//     })
 // }
+
+// getUserInfo("")
